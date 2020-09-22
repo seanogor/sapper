f4d86f4902ddc47622defa51d987309289d5436a
Seanogor <seanogor@gmail.com>
2020-09-22 06:29:53 +0100

    Update index.svelte
    topical change of comment

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -40,7 +40,7 @@
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
+	<figcaption>VERY NICE! I LIKE!!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
